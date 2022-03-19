7668b869fefffc84f4a6024ce0ba4024918df143
Benas Strauka <benas.strauka@telesoftas.com>
2022-03-19 15:03:20 +0200

    feat: project init.

---
+++ xsports/src/main/java/com/ktu/xsports/XsportsApplication.java
@@ -0,0 +1,13 @@
+package com.ktu.xsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsportsApplication.class, args);
+	}
+
+}
