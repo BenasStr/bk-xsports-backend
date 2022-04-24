1127d7d66ddb43dfafe05cc5be643c1765dcfc23
Benas Strauka <benas.strauka@telesoftas.com>
2022-04-24 14:19:57 +0300

    feat: Role name can not be null.

--- xsports/src/main/java/com/ktu/xsports/api/domain/Role.java
+++ xsports/src/main/java/com/ktu/xsports/api/domain/Role.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity(name = "roles")
 @Data
@@ -20,5 +21,6 @@ public class Role {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
     private String name;
 }
