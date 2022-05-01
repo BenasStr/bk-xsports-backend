d44a84a1d04ab24e601e4179258a24b939398e6d
Benas Strauka <benas.strauka@telesoftas.com>
2022-05-01 12:01:19 +0300

    feat: added CORS

--- xsports/src/main/java/com/ktu/xsports/XsportsApplication.java
+++ xsports/src/main/java/com/ktu/xsports/XsportsApplication.java
@@ -2,6 +2,9 @@ package com.ktu.xsports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class XsportsApplication {
@@ -10,4 +13,14 @@ public class XsportsApplication {
 		SpringApplication.run(XsportsApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/sports")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
