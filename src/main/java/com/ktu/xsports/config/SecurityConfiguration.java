1fa0ba8146cd6952fcec815879b58e6d8692f190
benas strauka <benas.strauka@telesoftas.com>
2023-03-26 15:00:11 +0300

    Feature: this is giving me cancer...

--- src/main/java/com/ktu/xsports/config/SecurityConfiguration.java
+++ src/main/java/com/ktu/xsports/config/SecurityConfiguration.java
@@ -128,7 +128,7 @@ public class SecurityConfiguration {
     private void imageEndpoints(HttpSecurity http) throws Exception {
         //User based requests
         http.authorizeHttpRequests()
-            .requestMatchers(GET, "/api/images/{fileName}").hasAnyAuthority(USER, ADMIN, MODERATOR)
+            .requestMatchers(GET, "/api/images/{fileName}").permitAll()
             .requestMatchers(POST, "/api/images/user").hasAnyAuthority(USER, ADMIN, MODERATOR)
             .requestMatchers(PUT, "/api/images/user").hasAnyAuthority(USER, ADMIN, MODERATOR)
             .requestMatchers(DELETE, "/api/images/user").hasAnyAuthority(USER, ADMIN, MODERATOR);
