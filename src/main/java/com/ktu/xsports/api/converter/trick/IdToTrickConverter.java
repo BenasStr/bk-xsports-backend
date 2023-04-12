f71a88099a876d415556acb01971e70e14746a96
benas strauka <benas.strauka@telesoftas.com>
2023-04-12 21:08:21 +0300

    Feature: converter changes.

--- src/main/java/com/ktu/xsports/api/converter/trick/IdToTrickConverter.java
+++ src/main/java/com/ktu/xsports/api/converter/trick/IdToTrickConverter.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.databind.util.StdConverter;
 import com.ktu.xsports.api.domain.Trick;
 import com.ktu.xsports.api.advice.exceptions.ServiceException;
 import com.ktu.xsports.api.repository.TrickRepository;
+import com.ktu.xsports.api.repository.TrickVariantRepository;
+import com.ktu.xsports.api.service.trick.TrickGroupService;
+import com.ktu.xsports.api.service.trick.TrickVariantService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class IdToTrickConverter extends StdConverter<Long, Trick> {
 
-    private final TrickRepository trickRepository;
+    private final TrickVariantRepository trickVariantRepository;
 
     @Override
     public Trick convert(Long id) {
-        return trickRepository.findById(id)
-            .orElseThrow(() -> new ServiceException(String.format("Trick with id: %d not found.", id)));
+        return trickVariantRepository.findById(id)
+            .orElseThrow(() -> new ServiceException(String.format("Trick with id: %d not found.", id)))
+            .getTrick();
     }
 }
--- src/main/java/com/ktu/xsports/api/converter/trick/IdsToTricksConverter.java
+++ src/main/java/com/ktu/xsports/api/converter/trick/IdsToTricksConverter.java
@@ -1,8 +1,10 @@
 package com.ktu.xsports.api.converter.trick;
 
 import com.fasterxml.jackson.databind.util.StdConverter;
+import com.ktu.xsports.api.advice.exceptions.ServiceException;
 import com.ktu.xsports.api.domain.Trick;
 import com.ktu.xsports.api.repository.TrickRepository;
+import com.ktu.xsports.api.repository.TrickVariantRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -11,14 +13,15 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class IdsToTricksConverter extends StdConverter<List<Long>, List<Trick>> {
 
-    private final TrickRepository trickRepository;
+    private final TrickVariantRepository trickVariantRepository;
 
     @Override
     public List<Trick> convert(List<Long> ids) {
         return ids.stream()
-                .map(id -> trickRepository
-                        .findById(id)
-                        .orElse(null))
-                .collect(Collectors.toList());
+                .map(id ->
+                    trickVariantRepository.findById(id)
+                        .orElseThrow(() -> new ServiceException("Trick not found!"))
+                        .getTrick())
+                .toList();
     }
 }
